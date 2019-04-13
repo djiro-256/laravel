3451cdd0bc67f1c0c03590481809fc6f398089c2
djiro-256 <info@d-nakazawa.com>
2019-04-13 23:05:49 +0900

    修正

--- Larval/database/migrations/2019_04_05_112029_create_posts_table.php
+++ Larval/database/migrations/2019_04_05_112029_create_posts_table.php
@@ -14,11 +14,11 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('title');
             $table->text('body');
             $table->timestamps();
-            $table->integer('user_id')->unsigned()->default(1);
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
