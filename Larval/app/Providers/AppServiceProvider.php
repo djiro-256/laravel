1bed470205656030ae837b40798f2237bc4a31de
djiro-256 <info@d-nakazawa.com>
2019-04-16 23:35:26 +0900

    fix templates

--- Larval/app/Providers/AppServiceProvider.php
+++ Larval/app/Providers/AppServiceProvider.php
@@ -27,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
+        if (\DB::getDriverName() == 'sqlite') {
+            \DB::statement(\DB::raw('PRAGMA foreign_keys=1'));
+        }
         
     }
 }
