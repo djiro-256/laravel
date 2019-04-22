9d6573f9f4ab71ce0ea60092559994398c2767a5
djiro-256 <info@d-nakazawa.com>
2019-04-22 22:48:06 +0900

    fix auth

--- Larval/app/Http/Controllers/PostController.php
+++ Larval/app/Http/Controllers/PostController.php
@@ -7,6 +7,11 @@ use App\Post;
 
 class PostController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -39,6 +44,7 @@ class PostController extends Controller
         $post = new Post;
         $post->title = $request->title;
         $post->body = $request->body;
+        $post->user_id = $request->user()->id;
         $post->save();
         return redirect('posts/'.$post->id);
     }
