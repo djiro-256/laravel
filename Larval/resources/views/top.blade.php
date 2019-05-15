9310bbffd0ad959f3f15c2b73f3ce29e918e2ec7
djiro-256 <info@d-nakazawa.com>
2019-05-15 20:08:18 +0900

    fix top.blade.php

--- Larval/resources/views/top.blade.php
+++ Larval/resources/views/top.blade.php
@@ -7,7 +7,10 @@
 @section('content')
 
 <div class="container-fluid text-white jumbotron">
-    <div class="bg-mask d-flex align-items-center">
+
+    <div class="bg-mask align-items-center">
+    <h1 class="text-center font-weight-bold pt-5">全国のレトロゲームショップが見つかるサイト</h1>
+        <br>
         <h1 class="text-white font-weight-bold text-center display-3 w-100">Retro Game Map(仮)</h1>
     </div>
 </div>
